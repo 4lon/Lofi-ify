298a3afa170465bda2f4a335d3eeb16e5a1c3824
Moss1410 <olive_macross@rocketmail.com>
2022-08-28 11:35:40 +1000

    file fixes

--- processing/lofi_processing.py
+++ processing/lofi_processing.py
@@ -1,7 +1,7 @@
 import os
 from pedalboard.io import AudioFile
 from pedalboard import Pedalboard, Chorus, Reverb, Bitcrush, Resample, Compressor, HighpassFilter, LowpassFilter, PitchShift
-import helpers
+import processing.helpers as helpers
 from midi2audio import FluidSynth
 
 
